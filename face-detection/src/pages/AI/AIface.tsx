317ab8c06281e0ce2bbbb1321e0b47b555da607e
tulhm <tulhm@vng.com.vn>
2023-03-06 11:29:04 +0700

    done: face detection

---
+++ face-detection/src/pages/AI/AIface.tsx
@@ -0,0 +1,66 @@
+import { useEffect, useMemo, useState } from "react";
+import React from "react";
+import useDetectFace from "./hooks/useDetectFace";
+
+
+const AIPage = () => {
+    // const [image, setImage] = useState<any>(null)
+    const { image, onImageChange, yaw, getYaw, status, human, time } = useDetectFace();
+    const [img, setImg] = useState<any>();
+    const [isPress, setIsPress] = useState(false);
+
+    // const onImageChange = (event:any) => {
+    //     if (event.target.files && event.target.files[0]) {
+    //         setImage(URL.createObjectURL(event.target.files[0]));
+
+    //         const img = document.getElementsByTagName('img')[0];
+    //         console.log("img", img);
+    //     }
+    // }
+
+
+    useEffect(() => {
+        if (image) {
+            console.log("image", image)
+        }
+    }, [image])
+
+    useEffect(()=>{
+        setImg(document.getElementsByTagName('img')[0]);
+    },[onImageChange])
+
+    return (
+        <div>
+            <h3>Nhập ảnh nha đồng chí</h3>
+
+            <br />
+
+            <input type='file' onChange={onImageChange} accept="image/png, image/jpeg, image/jpg" ></input>
+
+            <br /><br />
+
+            <img src={image} alt='abc' width={300} height={400} />
+
+            <br /><br />
+
+            {!isPress && <button onClick={() => { setIsPress(true); getYaw(img) }} style={{ backgroundColor: 'blue', border: '2px solid black', color: 'white', padding: '10px' }}>Get data</button>}
+
+            <br /><br />
+
+            <p>status: {status?.map((s)=><span key={s}>{s} - </span>)}</p>
+            <p>human version:{human?.version}, tfjs version: {human?.tf.version['tfjs-core']}</p>
+            <p>platform: {human?.env.platform}</p>
+            <p>agent: {human?.env.agent}</p>
+            <p>backend: {human?.tf?.getBackend()}, available: {human?.env?.backends?.map((be)=><span key={be}>{be} - </span>)}</p>
+            {human && <p>loaded models: {Object.values(human!.models).filter((model) => model !== null).length}</p>}
+
+            <br/><br/>
+            <p>--------------------------------</p>
+            <p>yaw: {yaw}</p>
+            <p>in: {time}ms</p>
+
+        </div>
+    )
+}
+
+export default AIPage;
---
+++ face-detection/src/pages/AI/hooks/useDetectFace.ts
@@ -0,0 +1,118 @@
+// import { Human } from '../../../dists/human.esm.js';
+import type { Human, Config } from '@vladmandic/human';
+import { useState, useCallback, useEffect } from 'react';
+
+const humanConfig: Partial<Config> = { // user configuration for human, used to fine-tune behavior
+    debug: true,
+    modelBasePath: 'https://cdn.jsdelivr.net/npm/@vladmandic/human@3.0.5/models/',
+    filter: { enabled: true, equalization: false, flip: false },
+    face: {
+        enabled: true,
+        detector: { rotation: false, maxDetected: 100, minConfidence: 0.2, return: true },
+        iris: { enabled: false },
+        description: { enabled: false },
+        emotion: { enabled: false },
+        antispoof: { enabled: false },
+        liveness: { enabled: false },
+    },
+    body: { enabled: false },
+    hand: { enabled: false },
+    object: { enabled: false },
+    gesture: { enabled: false },
+    segmentation: { enabled: false },
+};
+
+const deg = (rad: any) => Math.round((rad || 0) * 180 / Math.PI);
+
+const useDetectFace = () => {
+    const [time, setTime] = useState<any>(0);
+    const [image, setImage] = useState<any>('');
+    const [yaw, setYaw] = useState(0);
+    const [status, setStatus] = useState<string[]>([]);
+    // const [ready, setReady] = useState(false);
+    const [human, setHuman] = useState<Human | null>(null);
+
+    useEffect(() => {
+        const tmpList = status;
+        tmpList.push('init');
+        setStatus(tmpList)
+        import('@vladmandic/human').then((H) => {
+            const human = new H.default(humanConfig) as Human;
+            console.log('human version:', human.version, '| tfjs version:', human.tf.version['tfjs-core']);
+            console.log('platform:', human.env.platform, '| agent:', human.env.agent);
+
+            human.load().then(() => { // preload all models
+                const tmpList = status;
+                tmpList.push('models loaded');
+                setStatus(tmpList)
+                console.log('backend:', human!.tf.getBackend(), '| available:', human!.env.backends);
+                console.log('loaded models:' + Object.values(human!.models).filter((model) => model !== null).length);
+                human!.warmup().then(() => { // warmup function to initialize backend for future faster detection
+                    const tmpList = status;
+                    tmpList.push('warmup done');
+                    setStatus(tmpList)
+                    setHuman(human);
+                });
+            });
+        });
+    // eslint-disable-next-line
+    }, [])
+
+    // const init = useCallback(async () => {
+    //     console.log('\n\n');
+    //     console.log('human version:', human.version, '| tfjs version:', human.tf.version['tfjs-core']);
+    //     console.log('platform:', human.env.platform, '| agent:', human.env.agent);
+    //     console.log('loading...')
+    //     await human.load();
+    //     console.log('backend:', human.tf.getBackend(), '| available:', human.env.backends);
+    //     console.log('models stats:', human.models.stats());
+    //     console.log('models loaded:', human.models.loaded());
+    //     console.log('environment', human.env);
+    //     await human.warmup();
+    //     console.log('initializing...\n\n')
+    //     console.log('\n');
+    // }, [human])
+
+    const onImageChange = (event: any) => {
+        if (event.target.files && event.target.files[0]) {
+            const tmpList = status;
+            tmpList.push('image loaded');
+            setStatus(tmpList);
+            setImage(URL.createObjectURL(event.target.files[0]));
+        }
+    }
+
+    const getYaw = useCallback(async (imgElement: HTMLImageElement) => {
+        console.log("human", human);
+        console.log("imgElement", imgElement);
+        if (!human || !imgElement) return;
+
+        const tmpList = status;
+        // tmpList.push('start detecting');
+        // setStatus(tmpList);
+        console.time('detect');
+        const start = new Date().getTime();
+        const res = await human.detect(imgElement);
+        const end = new Date().getTime();
+        console.timeLog('detect', 'finish detection');
+        console.log("res", res);
+        setYaw(deg(res.face?.[0]?.rotation?.angle?.yaw));
+        tmpList.push('detecting done');
+        setStatus(tmpList);
+        setTime(end - start);
+    // eslint-disable-next-line
+    }, [human])
+
+    return {
+        // init,
+        image,
+        yaw,
+        onImageChange,
+        getYaw,
+        status,
+        human,
+        time
+    }
+}
+
+export default useDetectFace;
